30d9af44e3aa52cee63e24a1a8f5143e73d3656e
Gianluca Mazzini <gianluca@mazzini.org>
2018-03-01 05:10:43 +0100

    Update proc.php

--- proc.php
+++ proc.php
@@ -8,9 +8,12 @@ $data1=array();
 for($i=0;$i<$nfile;$i++){
   if($files[$i]=="."||$files[$i]=="..")continue;
   $fp=fopen($dir."/".$files[$i],"r");
-  fscanf($fp,"%ul %s %ul %d\n",$timestamp,$ip,$maca,$type);
-  $data1[$ip][$maca]++;
-  echo $ip." ";
+  for(;;){
+    fscanf($fp,"%ul %s %ul %d\n",$timestamp,$ip,$maca,$type);
+    if(feof($fp))break;
+    $data1[$ip][$maca]++;
+    echo $ip." ";
+  }
   fclose($fp);
 }
 
