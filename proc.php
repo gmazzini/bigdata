9f7033513b791d10abeff90060755f91ad7ba50c
Gianluca Mazzini <gianluca@mazzini.org>
2018-03-01 04:55:15 +0100

    Update proc.php

--- proc.php
+++ proc.php
@@ -1,9 +1,14 @@
 <?php
 
-$dir='/arubasyslog/log';
+$dir="/arubasyslog/log";
 $files=scandir($dir);
+$nfile=count($files);
 
-print_r($files);
+for($i=0;$i<$nfile;$i++){
+  if($files[$i]=="."||$files[$i]=="..")continue;
+  print($files[$i]);
+}
+
 
 
 ?>
