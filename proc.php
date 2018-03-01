641072e9ef24d13268228a66ab2cf7adb05d7e4b
Gianluca Mazzini <gianluca@mazzini.org>
2018-03-01 05:33:36 +0100

    Update proc.php

--- proc.php
+++ proc.php
@@ -3,7 +3,10 @@
 $dir="/arubasyslog/log";
 $files=scandir($dir);
 $nfile=count($files);
+error_reporting(0);
+
 $data1=array();
+$data2=array();
 
 for($i=0;$i<$nfile;$i++){
   if($files[$i]=="."||$files[$i]=="..")continue;
@@ -13,11 +16,13 @@ for($i=0;$i<$nfile;$i++){
     if(feof($fp))break;
     list($timestamp,$ip,$maca,$type)=$myline;
     $data1[$ip][$maca]++;
+    $data2[$maca]++;
   }
   fclose($fp);
 }
 
 print_r($data1);
+print_r($data2);
 
 
 ?>
