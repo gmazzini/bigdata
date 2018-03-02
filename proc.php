a05f3378d2d0511bdb6c121464ba48661c484ab9
Gianluca Mazzini <gianluca@mazzini.org>
2018-03-02 08:08:11 +0100

    Update proc.php

--- proc.php
+++ proc.php
@@ -39,6 +39,8 @@ $totuser=0;
 $totap=0;
 $minuser=10000000;
 $maxuser=0;
+$aveuserinbusy=0;
+$avetimeinbusy=0;
 foreach($data1 as $ip => $value ){
   $cc2=count($value);
   $totuser+=$cc2;
@@ -47,14 +49,18 @@ foreach($data1 as $ip => $value ){
   if($cc2>$maxuser)$maxuser=$cc2;
   $avetime=$data3[$ip]["acctime"]/$data3[$ip]["event"];
   $aveuser=$data3[$ip]["accuser"]/$data3[$ip]["event"];
+  $aveuserinbusy+=$avetime;
+  $avetimeinbusy+=$aveuser;
   $hostname=gethostbyaddr($ip);
   printf("%s %s %d %.2f %.2f\n",$ip,$hostname,$cc2,$aveuser,$avetime);
 }
-echo "UserUniqueAP=".$totuser."\n";
-echo "TotalUser=".count($data2)."\n";
-echo "TotAP=".$totap."\n";
+printf("UserUniqueAP=%d\n",$totuser);
+printf("TotalUser=%d\n",count($data2));
+printf("TotAP=%d\n",$totap);
 printf("UserperAP=%.2f\n",$totuser/$totap);
-echo "minUserAP=".$minuser."\n";
-echo "maxUserAP=".$maxuser."\n";
+printf("minUserAP=%d\n",$minuser);
+printf("maxUserAP=%d\n",$maxuser);
+printf("AveUserinBusy=%.2f\n",$aveuserinbusy/$totap);
+printf("AveTimainBusy=%.2f\n",$avetimeinbusy/$totap);
 
 ?>
